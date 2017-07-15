b66cae6d9d6928a57f994b46515f7fe1d2fff700
ponip <learningisnevercomplete@gmail.com>
2017-07-15 16:22:31 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,21 +2,21 @@
 
 ##makeCacheMatrix function returning a list of set,get,setInverse & getInverse function
 
-makeCacheMatrix <- function(x = matrix()) 
+makeCacheMatrix <- function(x = matrix())  #creating function and initializing x
 {
-  inv <- NULL 
-  set <- function(y) 
+  inv <- NULL #initializing inv
+  set <- function(y)  #set function
   {
-    x <<- y 
-    inv <<- NULL 
+    x <<- y #sets new matrix to cache
+    inv <<- NULL #sets inverse value in cache to null if new set of matrix is generated
   }
   
-  get <- function() x
-  setInverse <- function(inverse) inv <<- inverse
-  getInverse <- function() inv
-  list(set = set, get = get,
+  get <- function() x #get funtion returning the matrix in cache
+  setInverse <- function(inverse) inv <<- inverse #setInverse function for overwriting the new inverse value in cacheSolve
+  getInverse <- function() inv #getInverse function returning the inverse value
+  list(set = set, get = get, 
        setInverse = setInverse,
-       getInverse = getInverse)
+       getInverse = getInverse)  #returning the values as list for easy access in cacheSolve
 }
 
 
@@ -24,14 +24,14 @@ makeCacheMatrix <- function(x = matrix())
 
 cacheSolve <- function(x, ...)
 {
-  inv <- x$getInverse() 
-  if(!is.null(inv)) 
+  inv <- x$getInverse() #gets the matrix inverse
+  if(!is.null(inv)) #checks if the inverse value in cache is null
     {
-    message("getting cached data")
-    return(inv)
+    message("getting cached data")  #displaying the msg if the value of inv is not null
+    return(inv) #returning the inverse value 
     }
-  data <- x$get() 
-  inv <- solve(data, ...) 
-  x$setInverse(inv)
-  inv 
+  data <- x$get() #gets the matrix
+  inv <- solve(data, ...) #calculating the inverse of matrix
+  x$setInverse(inv) #sets the inverse of matrix to cache
+  inv #returning the inverse of matrix
 }
